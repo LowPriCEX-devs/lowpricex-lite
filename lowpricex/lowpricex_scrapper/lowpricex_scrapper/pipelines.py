44216a2f360d1a21676c3e8ab3f1eab7e1f1fb57
agubelu <agustinbd3@gmail.com>
2016-12-23 20:24:09 +0100

    Introducida la clave de la API en un archivo en gitignore
    Y versión inicial del pipeline que se encarga de buscar la info de los juegos en IGDB y meterlos en la BD.

--- lowpricex/lowpricex_scrapper/lowpricex_scrapper/pipelines.py
+++ lowpricex/lowpricex_scrapper/lowpricex_scrapper/pipelines.py
@@ -5,8 +5,19 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import requests
+from urllib.parse import quote_plus # URL Encoding
+from scrapy.exceptions import DropItem
+from logging import debug, info, warning, error 
+from api_keys import KEY_IGDB_API
+
 class ProcesadorJuegos(object):
 
     def process_item(self, item, spider):
-        #print("Juego: %s" % item["titulo"])
+        #Si el juego ya está en la BD, devolverlo directamente y terminar
+
+        data = obtenerInfoIGDB(item["titulo"])
         return item
+
+def obtenerInfoIGDB(titulo):
+    return {} #TODO

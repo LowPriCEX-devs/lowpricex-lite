50c7f48586e6e5cd9fa6e8ba696b93a27ca67fcf
agubelu <agustinbd3@gmail.com>
2016-12-24 18:18:55 +0100

    Añadidas opciones secretas en archivo en gitignore

--- lowpricex/lowpricex/settings.py
+++ lowpricex/lowpricex/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from . import secret_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^%396#xvp$+-x^jzw8!)=^txel@&g&b(2d884cp9yxf&^3h396'
+SECRET_KEY = secret_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +74,7 @@ WSGI_APPLICATION = 'lowpricex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = secret_settings.DATABASES_DATA
 
 
 # Password validation

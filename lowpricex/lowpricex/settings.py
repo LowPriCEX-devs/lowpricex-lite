534468bb44ecaa60aefdb9ea6439795389e46395
lowpricex <lowpricex@lowpricex.info>
2017-01-06 22:12:15 +0000

    Debug = false automático en el server

--- lowpricex/lowpricex/settings.py
+++ lowpricex/lowpricex/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "LOWPRICEX_DEPLOYMENT" not in os.environ
 
 ALLOWED_HOSTS = ["lowpricex.info", "www.lowpricex.info"]
 

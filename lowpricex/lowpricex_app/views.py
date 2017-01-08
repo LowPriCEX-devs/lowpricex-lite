cdbae84ffa8f436dd5e1e1e4629e1ade82d37e9a
David de los Santos Boix <ddlsb@hotmail.com>
2017-01-08 15:17:37 +0100

    Finalizado el fichero de Index

--- lowpricex/lowpricex_app/views.py
+++ lowpricex/lowpricex_app/views.py
@@ -3,7 +3,7 @@ from lowpricex_app.models import Plataforma, Juego
 from django.shortcuts import render
 
 from datetime import timedelta
-from django.db.models import ExpressionWrapper, DecimalField, F
+from django.db.models import ExpressionWrapper, FloatField, F
 
 def index(request):
     # Obtenemos las plataformas de búsqueda
@@ -22,13 +22,17 @@ def index(request):
     
     # Buscamos juegos cuya última fecha de actualización sea la fecha, y la fecha de histórico sea la de ayer, y ordenamos por diferencia
     juegosCambian = Juego.objects.filter(actualizado = fecha, historicojuego__fecha = fechaAyer). \
-                        annotate(difCompra = ExpressionWrapper(F('precioCompra') - F('historicojuego__precioCompra'), output_field=DecimalField())). \
-                        annotate(difVenta = ExpressionWrapper(F('precioVenta') - F('historicojuego__precioVenta'), output_field=DecimalField())). \
-                        annotate(difCompra = ExpressionWrapper(F('precioIntercambio') - F('historicojuego__precioIntercambio'), output_field=DecimalField()))
+                        annotate(difCompra = ExpressionWrapper(F('precioCompra') - F('historicojuego__precioCompra'), output_field=FloatField())). \
+                        annotate(difVenta = ExpressionWrapper(F('precioVenta') - F('historicojuego__precioVenta'), output_field=FloatField())). \
+                        annotate(difIntercambio = ExpressionWrapper(F('precioIntercambio') - F('historicojuego__precioIntercambio'), output_field=FloatField())). \
+                        annotate(precioCompraAyer = ExpressionWrapper(F('historicojuego__precioCompra'), output_field=FloatField())). \
+                        annotate(precioVentaAyer = ExpressionWrapper(F('historicojuego__precioVenta'), output_field=FloatField())). \
+                        annotate(precioIntercambioAyer = ExpressionWrapper(F('historicojuego__precioIntercambio'), output_field=FloatField()))
+                        
 
-    bajanCompra = juegosCambian.order_by('difCompra')[:5]
-    subenVenta = juegosCambian.order_by('-difVenta')[:5]
+    bajanVenta = juegosCambian.order_by('difVenta')[:5]
+    subenCompra = juegosCambian.order_by('-difCompra')[:5]
     subenIntercambio = juegosCambian.order_by('-difIntercambio')[:5]
     
     return render(request, 'index.html', {'plataformas':plataformas.order_by('nombre'), 'nintendo':nintendo, 'sony':sony, 'microsoft':microsoft, 'pc':pc, \
-                                          'bajanCompra':bajanCompra, 'subenVenta':subenVenta, 'subenIntercambio':subenIntercambio})
+                                          'subenCompra':subenCompra, 'bajanVenta':bajanVenta, 'subenIntercambio':subenIntercambio})
